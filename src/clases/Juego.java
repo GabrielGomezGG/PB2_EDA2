6f5e03efb3be8e4fb6095477da1fcebf7e714d0e
GabrielGomezGG <gabrielgomezgg1997@gmail.com>
2020-11-10 23:18:14 -0300

    3er Creo que termine

--- src/clases/Juego.java
+++ src/clases/Juego.java
@@ -3,8 +3,8 @@ package clases;
 import java.util.ArrayList;
 
 public class Juego {
-	ArrayList<Skin> personajes;
-	ArrayList<Skin> enemigos;
+	private ArrayList<Skin> personajes;
+	private ArrayList<Skin> enemigos;
 	
 	//TURNO
 	//false -> enemigos
@@ -13,17 +13,118 @@ public class Juego {
 	
 	public Juego(){
 		turno = true;
+		personajes = new ArrayList<Skin>();
+		enemigos = new ArrayList<Skin>();
 	}
 	
-	public Boolean batalla(Skin[] personajes, Skin[] enemigos){
+	/*
+	-Si las listas de personajes y enemigos no estan vacias
+	¡comienza la batalla!
+	-La batalla dura hasta que uno de los dos se quede sin "tropas" (.isEmpty() == true)
+	-En cada turno, al que le corresponda, ataca a su oponente, si este muere, 
+	se lo borra de la lista.
+	-Los que pelean en la lista, siempre seran los de la posicion [0].
+	-ganador:
+	true = personajes
+	false = false
+	*/
+	public Boolean batallaFinal(){
+		//if(!personajes.isEmpty() && !enemigos.isEmpty()){
+			//TURNO
+			//false -> enemigos
+			//true -> personaje
+		
+		Boolean siBatallaron = false;
+			
+			while(!personajes.isEmpty() && !enemigos.isEmpty()){
+				
+				siBatallaron = true;
+				
+				//Turno personajes
+				if(turno){
+					personajes.get(0).atacar(enemigos.get(0));
+					eliminarCaido(enemigos);
+				}
+				
+				//turno enemigos
+				if(!turno){
+					enemigos.get(0).atacar(personajes.get(0));
+					eliminarCaido(personajes);
+					
+					
+				}
+				cambiarTurno();
+			}
+			
+			if(siBatallaron){
+				if(personajes.isEmpty())
+					return false;
+				
+				if(enemigos.isEmpty())
+					return true;
+			}
+			
+			return false;
+		//}
+		
+	}
+	
+	//Agrega Personaje a la lista personajes igual a la cantidad ingresada
+	public boolean agregarPersonajes(Integer cantidad){
+			
+		if(cantidad > 0){
+			for(int i = 0; i < cantidad; i++){
+				personajes.add(new Personaje());
+			}
+			return true;
+		}
 		return false;
 	}
 	
-	public boolean agregarABatalla(Skin[] agro){
+	//Agregar enemigos a la lista, pero se puede agregar de distintos tipo:
+	//1: Goblins
+	//2: Ogro
+	//3: Dragon
+	//por defecto: goblins
+	public Boolean agregarEnemigos(Integer tipo, Integer cantidad){
+		
+		if(cantidad > 0){
+			switch (tipo) {
+				case 1: 
+					for(int i = 0; i < cantidad; i++){
+						enemigos.add(new Goblin());
+					}
+					break;
+				case 2: 
+					for(int i = 0; i < cantidad; i++){
+					enemigos.add(new Ogro());
+				}
+				break;
+				case 3: 
+					for(int i = 0; i < cantidad; i++){
+						enemigos.add(new Dragon());
+					}
+					break;
+				default:
+					for(int i = 0; i < cantidad; i++){
+						enemigos.add(new Goblin());
+					}
+					break;
+			}
+			return true;
+	    }
+		
 		return false;
 	}
 	
-	public boolean eliminarCaido(Skin[] caido){
+	//Elimina el primer elemento del array si es que 
+	//esta muerto (conVida = false).
+	public boolean eliminarCaido(ArrayList<Skin> caido){
+		
+		if(!caido.get(0).getConVida()){
+			caido.remove(0);
+			return true;
+		}
 		return false;
 	}
 	
@@ -32,6 +133,19 @@ public class Juego {
 			turno = false;
 		else
 			turno = true;
+	}
+	
+	public ArrayList<Skin> getPersonajes(){
+		return personajes;
+	}
+	
+	public ArrayList<Skin> getEnemigos(){
+		return enemigos;
+	}
+	
+	public void vaciarListas(){
+		personajes.clear();
+		enemigos.clear();
 	}
 	
 	
--- src/clases/TestJuego.java
+++ src/clases/TestJuego.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 public class TestJuego {
 	
 	@Test
-	public void Probando(){
+	public void ProbandoAtacar(){
+		
+		//Probando Atacar()
 		Skin pj1 = new Personaje();
 		Skin g1 = new Dragon();
 		
@@ -15,5 +17,58 @@ public class TestJuego {
 		pj1.atacar(g1);
 		
 		assertTrue(g1.getConVida());
+	}
+	
+	@Test
+	public void AgregandoPersonajes(){
+		//Agregar personaje
+		
+		Juego juego = new Juego();
+		
+		assertFalse(juego.agregarPersonajes(0));
+		assertTrue(juego.agregarPersonajes(5));
+	}
+		
+	@Test
+	public void EliminandoCaidos(){
+		
+		Dragon dragon = new Dragon();
+		Juego juego = new Juego();
+		
+		juego.agregarPersonajes(4);
+		
+		//El dragon de dos ataques elimina al personaje
+		
+		dragon.atacar(juego.getPersonajes().get(0));
+		dragon.atacar(juego.getPersonajes().get(0));
+		
+		juego.eliminarCaido(juego.getPersonajes());
+		//Elimino a 1
+		assertEquals(3, juego.getPersonajes().size());
+		
+		dragon.atacar(juego.getPersonajes().get(0));
+		dragon.atacar(juego.getPersonajes().get(0));
+		
+		juego.eliminarCaido(juego.getPersonajes());
+		//Elimino a otro
+		assertEquals(2, juego.getPersonajes().size());		
+	}
+	
+	@Test
+	public void BatallaFinal(){
+		
+		Juego juego = new Juego();
+		
+		juego.agregarPersonajes(1);
+		juego.agregarEnemigos(1, 3);
+		juego.agregarEnemigos(2, 1);
+		juego.agregarEnemigos(3, 3);
+		
+		//Un personaje VS 3 goblins, 1 ogro, 1 dragon
+		
+		assertFalse(juego.batallaFinal());	
+		//perdio el Personaje
+		
+		juego.vaciarListas();
 	}
 }
